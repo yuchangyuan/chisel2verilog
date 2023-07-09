0045cd7b79c1e8d65f8eb8f2e246942af49cae34
Yu Changyuan <reivzy@gmail.com>
2023-07-09 19:14:27 +0800

    Add support for firtool command line option

--- chisel2verilog.scala
+++ chisel2verilog.scala
@@ -4,15 +4,29 @@ import scala.reflect.runtime.{universe => ru}
 import chisel3._
 
 object run {
-    def main(args: Array[String]) {
-        val mirror: ru.Mirror = ru.runtimeMirror(getClass.getClassLoader)
+  def main(args: Array[String]) {
+    val mirror: ru.Mirror = ru.runtimeMirror(getClass.getClassLoader)
 
-        val classSymbol = mirror.staticClass(args(0))
-        val consMethodSymbol = classSymbol.primaryConstructor.asMethod
+    val classSymbol = mirror.staticClass(args(0))
+    val consMethodSymbol = classSymbol.primaryConstructor.asMethod
 
-        val classMirror = mirror.reflectClass(classSymbol)
-        val consMethodMirror = classMirror.reflectConstructor(consMethodSymbol)
+    val classMirror = mirror.reflectClass(classSymbol)
+    val consMethodMirror = classMirror.reflectConstructor(consMethodSymbol)
 
-        circt.stage.ChiselStage.emitSystemVerilogFile(consMethodMirror.apply().asInstanceOf[RawModule], args.drop(1))
+    var cargs: Seq[String] = Seq()
+    var fargs: Seq[String] = Seq()
+
+    var isCArgs = true
+
+    args.drop(1).foreach { a =>
+      if (a == "--") { isCArgs = false }
+      else {
+        if (isCArgs) { cargs = cargs :+ a }
+        else         { fargs = fargs :+ a }
+      }
     }
+
+    circt.stage.ChiselStage.emitSystemVerilogFile(consMethodMirror.apply().asInstanceOf[RawModule],
+      cargs.toArray, fargs.toArray)
+  }
 }
