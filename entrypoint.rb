fc8ab71a8ced8fa88e0155812f0a967424ce1d15
Yu Changyuan <reivzy@gmail.com>
2024-03-23 20:03:50 +0800

    entrypoint.rb: support filelist with '@' prefix

--- entrypoint.rb
+++ entrypoint.rb
@@ -19,10 +19,12 @@ end
 # -------------- split scala src & jar
 scala_src = []
 jar_src = []
+filelist_src = []
 
 srcs.each do |s|
   scala_src << s if s.end_with?(".scala")
   jar_src   << s if s.end_with?(".jar")
+  filelist_src << s if s.start_with?("@")
 end
 
 # ------------- compile
@@ -39,7 +41,7 @@ target_dir = Dir.mktmpdir("chisel2verilog")
 at_exit { FileUtils.remove_entry(target_dir) }
 
 compile_cmd = ["scalac", "-cp", classpath, "-Xplugin:#{plugin_jar}",
-               "-d", target_dir] + scala_src
+               "-d", target_dir] + scala_src + filelist_src
 
 
 system(compile_cmd.join(" ")) or begin
